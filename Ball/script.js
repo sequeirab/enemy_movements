fac30a6b617779b23b4408df2a255f98507b821a
Bruno Sequeira <sequeirab86@gmail.com>
2022-08-05 16:19:48 +0100

    everything working

--- Ball/script.js
+++ Ball/script.js
@@ -11,27 +11,32 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.enemyImage = new Image();
-        this.enemyImage.src = '../img/enemy3.png';
+        this.enemyImage.src = '../img/enemy4.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth / 2;
         this.height = this.spriteHeight / 2;
         this.x = Math.random() * (CANVAS_WIDTH - this.width);
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+        this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+        this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 100;
-        this.angleSpeed = Math.random() * 2;
-        //this.curve = Math.random() * 200 + 50;
+        this.interval = Math.floor(Math.random() * 200 + 50);
     }
     draw() {
         ctx.drawImage(this.enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
     update() {
-        this.x = CANVAS_WIDTH / 2 * Math.sin(this.angle * Math.PI/180) + (CANVAS_WIDTH / 2 - this.width / 2);
-        this.y = CANVAS_HEIGHT / 2 * Math.cos(this.angle * Math.PI/360) + (CANVAS_HEIGHT / 2 - this.height / 2);
-        this.angle += this.angleSpeed;
+        if(gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+            this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx / 70;
+        this.y -= dy / 70;
         if(this.x + this.width < 0) this.x = CANVAS_WIDTH;
         if(gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;    
