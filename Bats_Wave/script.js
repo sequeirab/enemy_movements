807357cca74a1a0f0decfedbd1a02d249415550c
Bruno Sequeira <sequeirab86@gmail.com>
2022-08-05 15:40:39 +0100

    bats wave

--- Bats_Wave/script.js
+++ Bats_Wave/script.js
@@ -12,22 +12,27 @@ class Enemy {
     constructor(){
         this.enemyImage = new Image();
         this.enemyImage.src = '../img/enemy2.png';
-        this.speed = Math.random() * 4 - 1;
+        this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 266;
         this.spriteHeight = 188;
-        this.width = this.spriteWidth / 2.5;
-        this.height = this.spriteHeight / 2.5;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
         this.x = Math.random() * (CANVAS_WIDTH - this.width);
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 10 + 4);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
     draw() {
         ctx.drawImage(this.enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
     update() {
         this.x -= this.speed;
-        //this.y -= this.speed;
+        this.y -= this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if(this.x + this.width < 0) this.x = CANVAS_WIDTH;
         if(gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;    
         }
